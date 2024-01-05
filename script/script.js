091120fd59bfedf11c510d0fcfa51223624c30fb
BrunoConceica0 <bd687353@gmail.com>
2024-01-04 22:32:47 -0300

    Tab com Navegação transformado em classe

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import inintDropDowMenu from "./modules/dropdow-menu.js";
 import ScrollSuave from "./modules/scroll-suave.js";
-import iniTabNav from "./modules/navegacao-por-tab.js";
+import TabNav from "./modules/navegacao-por-tab.js";
 import Accordion from "./modules/accordionList.js";
 import initAnimacaoScroll from "./modules/animacaoAoScroll.js";
 import inintFetchAnimais from "./modules/fetch-animais.js";
@@ -12,10 +12,14 @@ const scrollSauve = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 const accordion = new Accordion(" [data-anime='acction'] dt");
 scrollSauve.init();
 accordion.init();
+const tabNav = new TabNav(
+  "[data-tab='content'] section",
+  "[data-tab='menu'] li"
+);
+tabNav.init();
 initModal();
 initTooltip();
 inintDropDowMenu();
-iniTabNav();
 initAnimacaoScroll();
 inintFetchAnimais();
 initFetchBitcoin();
