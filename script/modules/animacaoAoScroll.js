d97ec7a4c48f0ef385044bf87d0bffc95ee2e68a
BrunoConceica0 <bd687353@gmail.com>
2024-01-10 20:35:50 -0300

    Animaçã ao Scroll Refeita

--- script/modules/animacaoAoScroll.js
+++ script/modules/animacaoAoScroll.js
@@ -2,20 +2,50 @@ export default class AnimacaoScroll {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.metadeTela = window.innerHeight * 0.6;
-    this.windowScroll = this.windowScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
-  windowScroll() {
-    this.sections.forEach((section) => {
-      const sectionsTop = section.getBoundingClientRect().top - this.metadeTela;
-      if (sectionsTop < 0) section.classList.add("ativo");
-      else if (section.classList.contains("ativo")) {
-        section.classList.remove("ativo");
+  // pega a distância em cada item em relação ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: offset - Math.floor(this.metadeTela),
+      };
+    });
+  }
+  // Verfica a distancia em cada objeto em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add("ativo");
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.remove("ativo");
       }
     });
   }
 
+  // windowScroll() {
+  //   this.sections.forEach((section) => {
+  //     const sectionTop = section.getBoundingClientRect().top - this.metadeTela;
+  //     if (sectionTop < 0) {
+  //       section.classList.add("ativo");
+  //     } else {
+  //       section.classList.remove("ativo");
+  //     }
+  //   });
+  // }
+
   init() {
-    this.windowScroll();
-    window.addEventListener("scroll", this.windowScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this;
+  }
+  // remove o evento do scroll
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
