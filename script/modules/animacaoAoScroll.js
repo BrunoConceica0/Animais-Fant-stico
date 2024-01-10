3ecbe9a1797311eaefa1b370a494976b2b26e49e
BrunoConceica0 <bd687353@gmail.com>
2024-01-10 19:15:56 -0300

    Anima Scroll Refatorado em classe

--- script/modules/animacaoAoScroll.js
+++ script/modules/animacaoAoScroll.js
@@ -1,15 +1,21 @@
-export default function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('[data-anime="scroll"]');
-
-  function windowScroll() {
-    sections.forEach((section) => {
-      const metadeTela = window.innerHeight * 0.6;
-      const sectionsTop = section.getBoundingClientRect().top - metadeTela;
+export default class AnimacaoScroll {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.metadeTela = window.innerHeight * 0.6;
+    this.windowScroll = this.windowScroll.bind(this);
+  }
+  windowScroll() {
+    this.sections.forEach((section) => {
+      const sectionsTop = section.getBoundingClientRect().top - this.metadeTela;
       if (sectionsTop < 0) section.classList.add("ativo");
       else if (section.classList.contains("ativo")) {
         section.classList.remove("ativo");
       }
     });
   }
-  window.addEventListener("scroll", windowScroll);
+
+  init() {
+    this.windowScroll();
+    window.addEventListener("scroll", this.windowScroll);
+  }
 }
