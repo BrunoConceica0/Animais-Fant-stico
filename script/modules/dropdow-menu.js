7e13c0cb455d1b30bea15fdfb6ba7a262900961d
BrunoConceica0 <bd687353@gmail.com>
2024-01-10 23:40:15 -0300

    Colocando algumas strings para explica cada função

--- script/modules/dropdow-menu.js
+++ script/modules/dropdow-menu.js
@@ -1,14 +1,16 @@
 import outsideClick from "./outsideClick.js";
 export default function initDropMenu() {
   const dropMenu = document.querySelectorAll("[data-dropdown]");
+
   function handleClick(event) {
     event.preventDefault();
     this.classList.toggle("active");
+    //  ["click", "touchstart"] são dois evento com o mesmo objecivo porem um pc pc e outro para smartfone
     outsideClick(this, ["click", "touchstart"], () => {
       this.classList.remove("active");
     });
   }
-
+  // adicionar forEach para cada elemento, usando um novo evento para todos
   dropMenu.forEach((menu) => {
     ["click", "touchstar"].forEach((userEvents) => {
       menu.addEventListener(userEvents, handleClick);
--- script/modules/outsideClick.js
+++ script/modules/outsideClick.js
@@ -1,21 +1,24 @@
+// paramentros dos elementos, dos eventos de click e touchstar e o callback, para fazer o evento de bubble
 export default function outsideClick(element, events, callback) {
   const html = document.documentElement;
   const outside = "data-outside";
 
+  //conferindo se não existe o atributo, se não existir, executará o evento
   if (!element.hasAttribute(outside)) {
     events.forEach((userEvents) => {
       html.addEventListener(userEvents, handleOutsideClick);
     });
+    // atribuindo o difinição para que quando for clicado, o evento do click não aculumar
     element.setAttribute(outside, "");
   }
+  // ao click para fora do elemento, o conteúdo vai sair
   function handleOutsideClick(event) {
+    // se o elemento não conter o target do evento, a condição fará o conteúdo sair
     if (!element.contains(event.target)) {
       events.forEach((userEvents) => {
         html.removeEventListener(userEvents, handleOutsideClick);
       });
-
       element.removeAttribute(outside, "");
-
       callback();
     }
   }
