e16451620d1f36fc12ee216570de48a1b7aea58c
BrunoConceica0 <bd687353@gmail.com>
2024-01-10 23:25:17 -0300

    Retrabalando no Dropdown Menu

--- script/modules/dropdow-menu.js
+++ script/modules/dropdow-menu.js
@@ -1,28 +1,17 @@
-export default function inintDropDowMenu() {}
-const dropDownMenu = document.querySelectorAll("[data-dropdown]");
-function outisideClick(element, event, callback) {
-  const html = document.documentElement;
-  const outside = "data-outside";
-  if (!element.hasAttribute(outside, "")) {
-    event.forEach((userEvent) => {
-      html.removeEventListener(userEvent, handleOutsideClick);
+import outsideClick from "./outsideClick.js";
+export default function initDropMenu() {
+  const dropMenu = document.querySelectorAll("[data-dropdown]");
+  function handleClick(event) {
+    event.preventDefault();
+    this.classList.toggle("active");
+    outsideClick(this, ["click", "touchstart"], () => {
+      this.classList.remove("active");
     });
-    html.addEventListener("click", handleOutsideClick);
-    element.setAttribute(outside, "");
   }
-}
 
-function handleClick(event) {
-  event.preventDefault(); // prevene que ao click acontece alguam coisa
-  this.classList.add("active"); // acitve é do css para dar display:block; no submenu
-  outisideClick(this, ["touchstart", "click"], () => {
-    // crie um function detro da function
-    this.classList.remove("active"); //  remove a classe do active para quando click, o evento ocorra apenas uma vez no site
+  dropMenu.forEach((menu) => {
+    ["click", "touchstar"].forEach((userEvents) => {
+      menu.addEventListener(userEvents, handleClick);
+    });
   });
 }
-//   ["touchstart", "click"] são dois evento com o mesmo objecivo porem um pc pc e outro para smartfone
-dropDownMenu.forEach((menu) => {
-  ["touchstart", "click"].forEach((userEvent) => {
-    menu.addEventListener(userEvent, handleClick);
-  });
-});
--- script/modules/outsideClick.js
+++ script/modules/outsideClick.js
@@ -1,1 +1,22 @@
+export default function outsideClick(element, events, callback) {
+  const html = document.documentElement;
+  const outside = "data-outside";
 
+  if (!element.hasAttribute(outside)) {
+    events.forEach((userEvents) => {
+      html.addEventListener(userEvents, handleOutsideClick);
+    });
+    element.setAttribute(outside, "");
+  }
+  function handleOutsideClick(event) {
+    if (!element.contains(event.target)) {
+      events.forEach((userEvents) => {
+        html.removeEventListener(userEvents, handleOutsideClick);
+      });
+
+      element.removeAttribute(outside, "");
+
+      callback();
+    }
+  }
+}
